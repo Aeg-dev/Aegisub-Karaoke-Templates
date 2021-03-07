7778e6375aee8c55023ca915dc82c9f4f4599cac
woclass <inkydragon@users.noreply.github.com>
2021-03-07 20:02:10 +0800

    [afv] 0.91

---
+++ Ass-Fx-Viewer/log.cmd
@@ -0,0 +1,1 @@
+START "" /B mongoose.exe >> mg.log
--- Ass-Fx-Viewer/点我打开网页.cmd
+++ Ass-Fx-Viewer/点我打开网页.cmd
@@ -1,2 +1,7 @@
-START "" /B mongoose.exe >> mg.log
-START ""    "http://localhost:2333"
+@echo off
+TITLE ASS FX Viewer - log output
+START "" /B  python-3.9.2-embed-win32\python.exe -m http.server 2333 --bind 127.0.0.1 --directory html
+ECHO WEB Server ON
+ECHO Opening webpage...
+START ""  "http://localhost:2333"
+@echo on
